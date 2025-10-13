92ac1a79a04122762171f64a1af3453d2d87c39f
김명진 <namj790710@gogle.com>
2025-10-13 17:22:34 +0900

    이용요금

--- src/components/main/Price.vue
+++ src/components/main/Price.vue
@@ -1,6 +1,22 @@
 <template>
-  <div class="price">이용요금!!!!</div> 
+  <h3 class="price">요금안내</h3>
+  <div class="notice">
+    <div>
+      무인 (개인보관함)
+      <li>기본요금 4시간</li>
+      <div class="price_info">냉장실 3000원</div>
+      <div class="price_info">상온 2500원</div>
+      <div class="price_info">4시간 초과 300원 / 10분당</div>
+       <div class="price_info">최대보관시간 24시간</div>
+    </div>
+    <div>
+      기사 (접수대행)
+      <div class="price_info">냉장실 4000원</div>
+      <div class="price_info">상온 3000원</div>
+      <div class="price_info">4시간 초과 300원 / 10분당</div>
+       <div class="price_info">최대보관시간 24시간</div>
+    </div>
+  </div>
 </template>
 <script setup></script>
-
-<style lang="scss" scoped></style>
+<style scoped></style>
