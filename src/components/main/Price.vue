42b896dc329fa52de3807beda545b0a41873d05f
김명진 <namj790710@gogle.com>
2025-10-13 17:26:37 +0900

    이용요금 작업중

--- src/components/main/Price.vue
+++ src/components/main/Price.vue
@@ -7,14 +7,14 @@
       <div class="price_info">냉장실 3000원</div>
       <div class="price_info">상온 2500원</div>
       <div class="price_info">4시간 초과 300원 / 10분당</div>
-       <div class="price_info">최대보관시간 24시간</div>
+      <div class="price_info">최대보관시간 24시간</div>
     </div>
     <div>
       기사 (접수대행)
       <div class="price_info">냉장실 4000원</div>
       <div class="price_info">상온 3000원</div>
       <div class="price_info">4시간 초과 300원 / 10분당</div>
-       <div class="price_info">최대보관시간 24시간</div>
+      <div class="price_info">최대보관시간 24시간</div>
     </div>
   </div>
 </template>
