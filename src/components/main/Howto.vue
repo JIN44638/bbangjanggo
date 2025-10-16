8bdb15f904f2fb072a3906ece989a07b688d7613
kimmyungjin <namj790710@gmail.com>
2025-10-16 15:30:34 +0900

    반응형 적용

--- src/components/main/Howto.vue
+++ src/components/main/Howto.vue
@@ -3,22 +3,29 @@
     <!-- 이용방법 -->
     <h1>이용방법</h1>
     <div class="inner">
-       <ul>
-        <div class="step1_2_3_4">
-       
-        <li>
-          <img src="/public/images/kmj/step1.png" alt="" />
-        </li>
-        <li>
-          <img src="/public/images/kmj/step2.png" alt="" />
-        </li>
-        <li>
-          <img src="/public/images/kmj/step3.png" alt="" />
-        </li>
-        <li>
-          <img src="/public/images/kmj/step4.png" alt="" />
-        </li>
-      </div></ul>
+      <!-- 웹 -->
+      <div class="step1_2_3_4">
+        <ul class="web">
+          <li>
+            <img src="/public/images/kmj/step1.png" alt="" />
+          </li>
+          <li>
+            <img src="/public/images/kmj/step2.png" alt="" />
+          </li>
+          <li>
+            <img src="/public/images/kmj/step3.png" alt="" />
+          </li>
+          <li>
+            <img src="/public/images/kmj/step4.png" alt="" />
+          </li>
+        </ul>
+      </div>
+      <!-- 모바일 -->
+      <div class="step1_2_3_4">
+        <ul class="mobile">
+          <img src="/public/images/kmj/howto-mobile.png" alt="모바일 이용방법" />
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -26,25 +33,43 @@
 
 <style lang="scss" scoped>
 .howto {
+  display: grid;
+  background-color: #ba8e5f;
 
-  background-color: #BA8E5F;
-
-  h1{
+  h1 {
     padding-top: 50px;
     text-align: center;
     color: #fff;
     margin-bottom: 60px;
-  }// width: 1000px;
+  } // width: 1000px;
 }
-.step1_2_3_4 {
-  // padding-top: 150px;
-  width: 100%;
-  display: flex;
-  gap: 48px;
-  flex: 1;
-    padding-bottom: 50px;
+/* 기본: PC 이미지만 보이게 */
+.web {
+   display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    gap: 20px; /* 칸 사이 간격 */
+    li{
+      img{
+        width: 100%;
+      }
+      padding-bottom: 50px;
+    }
 }
-img{
-  width: 100%;
+.mobile {
+  display: none;
+}
+
+/* 모바일 화면일 때 토글 */
+@media (max-width: 768px) {
+  .web {
+    display: none;
+  } /* PC 이미지 숨김 */
+  .mobile {
+    img {
+      width: 100%;
+    }
+    display: block;
+    padding-bottom: 80px;
+  } /* 모바일 이미지 보여줌 */
 }
 </style>
